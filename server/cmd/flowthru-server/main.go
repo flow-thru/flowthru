e1caa7289a08fe4fd7bc695ef73f2180dc674e97
Oscar Olazabal <oscarolazabal.dev@gmail.com>
2019-09-16 00:05:07 -0700

    remove old

--- server/cmd/flowthru-server/main.go
+++ server/cmd/flowthru-server/main.go
@@ -9,7 +9,7 @@ import (
 
 func main() {
     router := rest.Handler()
-    fmt.Println("data is now flowing thru: http://localhost:3000")
-	log.Fatal(http.ListenAndServe(":3000", router))
+    fmt.Println("flowing thru at http://localhost:5000")
+	log.Fatal(http.ListenAndServe(":5000", router))
 }
 
