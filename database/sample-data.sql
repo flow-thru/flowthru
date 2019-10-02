11d3578b25a929d8c28d42acc8eda48eddf61133
jswerrett <jwolfw@csu.fullerton.edu>
2019-10-01 20:10:04 -0700

    Added all dummy emails needed.

--- database/sample-data.sql
+++ database/sample-data.sql
@@ -1,6 +1,6 @@
 INSERT INTO public."user"(
 	id, first_name)
-	VALUES (1, 'adam');
+	VALUES (1, 'adam', 'test@flowthru.com');
 
 INSERT INTO public."user"(
 	id, first_name, primary_email)
@@ -8,15 +8,15 @@ INSERT INTO public."user"(
 
 INSERT INTO public."user"(
 	id, first_name)
-	VALUES (3, 'charlie');
+	VALUES (3, 'charlie', 'test@flowthru.com');
 
 INSERT INTO public."user"(
 	id, first_name)
-	VALUES (4, 'david');
+	VALUES (4, 'david', 'test@flowthru.com');
 
 INSERT INTO public."user"(
 	id, first_name)
-	VALUES (5, 'eugene');
+	VALUES (5, 'eugene', 'test@flowthru.com');
 
 INSERT INTO public.organization(
 	id, owner_id, name)
