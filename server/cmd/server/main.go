9fa08547c2285fc43e0c5fcb2e6d3d4f185f9318
Oscar Olazabal <oscarolazabal.dev@gmail.com>
2019-10-10 19:47:41 -0700

    add protected endpoints

--- server/cmd/server/main.go
+++ server/cmd/server/main.go
@@ -6,28 +6,29 @@ import (
     "os"
     "net/http"
 	"server/internal/http/rest"
-	"server/internal/platform/database/pg"
+	"server/internal/platform/database/pgsql"
+	"server/internal/platform/auth"
 )
 
 var (
-    DBCreds = fmt.Sprintf(
+    pgsqlCreds = fmt.Sprintf(
         "host=%s port=%s user=%s password=%s dbname=%s sslmode=disable",
         os.Getenv("FLOWTHRU_DB_HOST"),
         os.Getenv("FLOWTHRU_DB_PORT"),
         os.Getenv("FLOWTHRU_DB_USER"),
         os.Getenv("FLOWTHRU_DB_PASSWORD"),
-        os.Getenv("FLOWTHRU_DB_NAME"),
-    )
+        os.Getenv("FLOWTHRU_DB_NAME"))
+    auth0Domain = os.Getenv("FLOWTHRU_AUTH0_DOMAIN")
+    auth0Audience = os.Getenv("FLOWTHRU_API_IDENTIFIER")
 )
 
-
 func main() {
-    _, err := pg.NewDB(DBCreds)
-
+    _, err := pgsql.NewDB(pgsqlCreds)
     if err != nil {
         log.Fatal(err)
     }
-    router := rest.Handler()
+    userAuth := auth.NewUserAuth(auth0Domain, auth0Audience)
+    router := rest.Handler(userAuth)
     http.Handle("/", router)
     fmt.Println("flowing thru at http://localhost:5000")
 	log.Fatal(http.ListenAndServe(":5000", nil))
