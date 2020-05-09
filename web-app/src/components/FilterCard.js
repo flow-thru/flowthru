7aef7c3499a958177dd8c48fbb913a31229928fd
Oscar Olazabal <oscarolazabal.dev@gmail.com>
2020-05-09 02:20:12 -0700

    use radio buttons

--- web-app/src/components/FilterCard.js
+++ web-app/src/components/FilterCard.js
@@ -1,6 +1,6 @@
 
 import React from 'react'
-import { Card, Space, Button, Input } from 'antd';
+import { Card, Space, Radio, Input } from 'antd';
 import { FireOutlined, TeamOutlined, HistoryOutlined } from '@ant-design/icons';
 
 class FilterCard extends React.Component {
@@ -10,19 +10,20 @@ class FilterCard extends React.Component {
                 size='small'
                 style={{ width: 700 }}
             >
-                <Space size='large'>
-                    <Button shape='round'>
-                        <span><FireOutlined /> Popular </span>
-                    </Button>
-                    <Button shape='round'>
-                        <span><TeamOutlined /> Active</span>
-                    </Button>
-                    <Button shape='round'>
-                        <span><HistoryOutlined /> New</span>
-                    </Button>
-                    <Input.Search placeholder='Tag (e.g. "outdoors", "food")'/>
-                </Space>
-
+                <Radio.Group defaultValue="popular">
+                    <Space size='large'>
+                        <Radio.Button value="popular" style={{'border-radius': '15px'}}>
+                            <span><FireOutlined /> Popular </span>
+                        </Radio.Button>
+                        <Radio.Button value="active" style={{'border-radius': '15px'}}>
+                            <span><TeamOutlined /> Active</span>
+                        </Radio.Button>
+                        <Radio.Button value='new' style={{'border-radius': '15px'}}>
+                            <span><HistoryOutlined /> New</span>
+                        </Radio.Button>
+                    </Space>
+                </Radio.Group>
+                <Input.Search placeholder='Tags (e.g. "outdoors", "food")' style={{width:'40%', float:'right'}}/>
             </Card>
     )};
 }
